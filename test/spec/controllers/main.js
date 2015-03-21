2ec1acbc5483945008fce8f16039ca5b662352a5
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-21 15:17:08 +0000

    tested and working

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,13 +2,11 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('mytodoApp'));
 
   var MainCtrl,
     scope;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
@@ -16,7 +14,21 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
   });
+
+  it('should add items to the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+
+  it('should add then remove an item from the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(0);
+  });
+
 });
